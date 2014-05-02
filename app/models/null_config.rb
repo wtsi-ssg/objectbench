a8d90b4e031079e12620423416f76227770de65d
James Beal <James.Beal@sanger.ac.uk>
2014-05-02 09:09:18 +0100

    Revert to c47d5cddd08e179eebbbfb864d660f2ecfee90bc

--- app/models/null_config.rb
+++
@@ -1,2 +0,0 @@
-class NullConfig < ActiveRecord::Base
-end
--- test/models/null_config_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NullConfigTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
